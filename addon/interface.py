0c3a45e7762c3b6697494e8e1b5417c073e82154
trentinfrederick <trentin.shaun.frederick@gmail.com>
2016-11-14 17:30:27 -0600

    update

--- addon/interface.py
+++ addon/interface.py
@@ -14,7 +14,13 @@ def draw(self, context):
 
     row = column.row(align=True)
 
-    row.prop(context.scene.selection_bounds, 'color', text='')
+    if context.scene.selection_bounds.use_object_color:
+
+      row.prop(context.object, 'color', text='')
+
+    else:
+
+      row.prop(context.scene.selection_bounds, 'color', text='')
 
     row.prop(context.scene.selection_bounds, 'use_object_color', text='', icon='OBJECT_DATA')
 
